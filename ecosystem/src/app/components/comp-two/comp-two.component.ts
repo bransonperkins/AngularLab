046947081768bfa6b7f252310cbeafe892425da3
bransonperkins <bransonperkins@gmail.com>
2020-05-28 12:31:15 -0400

    added get, show and hide buttons

--- ecosystem/src/app/components/comp-two/comp-two.component.ts
+++ ecosystem/src/app/components/comp-two/comp-two.component.ts
@@ -8,21 +8,41 @@ import { Person } from '../../models/person';
   styleUrls: ['./comp-two.component.css']
 })
 export class CompTwoComponent implements OnInit {
+  public show: boolean = false;
+  public buttonName: any = 'Show';
+
   people: Person[];
 
   constructor(private personService: PersonService) { }
 
   ngOnInit(): void {
-    this.getPeople();
+    this.toggle();
   }
 
   getPeople(): void {
+    this.delay(4000);
     this.personService.getPerson().subscribe(
       (response) => {
         this.people = response;
-        console.log('observable is working'); },
+        console.log('observable is working' + this.people); },
       (error) => { console.log(error); }
     );
+  }
+
+  async delay(ms: number) {
+    await new Promise(resolve => setTimeout(() => resolve(), ms));
+  }
+
+  toggle() {
+    this.show = !this.show;
+
+    if (this.show) {
+      this.buttonName = 'Get People';
+    }
+    else {
+      this.getPeople();
+      this.buttonName = 'Show People';
+    }
   }
 
 }
--- ecosystem/src/app/services/person.service.ts
+++ ecosystem/src/app/services/person.service.ts
@@ -11,14 +11,15 @@ export class PersonService {
     new Person('Branson', 'Perkins', 26),
     new Person('Caleb', 'Perkins', 22),
     new Person('Kenny', 'Perkins', 49),
-    new Person('Jennifer', 'Perkins', 48)
+    new Person('Jennfer', 'Perkins', 48)
   ];
 
   constructor() { }
 
   getPerson(): Observable<Person[]> {
      return of(this.personArr.filter((person) => {
-       return ('Name: ' + person.getFullName() + ', Age: ' + person.getAge());
+      return ('Name: ' + person.getFullName() + ', Age: ' + person.getAge());
      }));
   }
+
 }
